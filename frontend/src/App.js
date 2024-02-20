12872057b406a0f7ed23461f9bb573b2fe2c0630
rathuparnna <rathuparnna.pc@emvigotech.com>
2024-02-20 15:43:23 +0530

    header and footer addded

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import { Container } from "react-bootstrap";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+
 
 function App() {
   return (
-    <div><h1>E-Commerce</h1>
-      </div>
-  )
-}
+    <>
+    <Header />
+    <main className="py-3">
+      <Container>
+    <h1>E-Commerce</h1>;
+    </Container>
+    </main>
+    <Footer />
+      </>
+  );
+};
 
 export default App
