a042e4bab04bbbdc86d9747dbcb41ac8298f025a
rathuparnna <rathuparnna.pc@emvigotech.com>
2024-03-13 17:29:17 +0530

    cart view set up

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, createRoutesFromElements,Route,RouterProvider} from "react-router-dom"
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom"
 import { Provider } from 'react-redux';
 import store from './store';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -10,13 +10,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
+
 
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<App/>}>
-       <Route index={true} path='/' element={<HomeScreen/>}></Route>
-       <Route path='/product/:id' element={<ProductScreen/>}></Route>
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<HomeScreen />}></Route>
+      <Route path='/product/:id' element={<ProductScreen />}></Route>
+      <Route path='/cart' element={<CartScreen />}></Route>
     </Route>
   )
 )
@@ -25,7 +28,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
---
+++ frontend/src/slices/cartSlice.js
@@ -0,0 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { updateCart } from "../utils/cartUtils";
+
+const initialState = {
+    cartItems: localStorage.getItem("cart")
+        ? JSON.parse(localStorage.getItem("cart"))
+        : []
+};
+
+const addDecimals = (num) => {
+    return (Math.round(num * 100) / 100).toFixed(2);
+};
+
+const cartSlice = createSlice({
+    name: "cart",
+    initialState,
+    reducers: {
+        addToCart: (state, action) => {
+            const item = action.payload;
+            const existItem = state.cartItems.find((x) => x._id === item._id);
+            if (existItem) {
+                state.cartItems = state.cartItems.map((x) =>
+                    x._id === existItem._id ? item : x
+                );
+            } else {
+                state.cartItems.push(item);
+            }
+            // Save updated cart to localStorage
+            localStorage.setItem("cart", JSON.stringify(state.cartItems));
+        },
+        removeFromCart: (state, action) => {
+            state.cartItems = state.cartItems.filter((x) => x._id !== action.payload);
+        }
+    }
+});
+
+export const { addToCart, removeFromCart } = cartSlice.actions;
+export default cartSlice.reducer;
